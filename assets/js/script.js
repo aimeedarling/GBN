54e8592f5cc1d76406e6b182afeb873dbefc0893
fishy <miah.nishat.227@gmail.com>
2023-05-09 18:12:52 -0400

    removed js code that didn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,1 @@
 
-GET https://newsapi.org/v2/everything?q=Apple&from=2023-05-09&sortBy=popularity&apiKey=633e9fc1f7394f1a90b345edcdbd4892
-
-var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2023-05-09&' +
-          'sortBy=popularity&' +
-          'apiKey=633e9fc1f7394f1a90b345edcdbd4892';
-
-var req = new Request(url);
-
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })

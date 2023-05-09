11273d28f7a77f32542b9d0967aad2a959da3c68
fishy <miah.nishat.227@gmail.com>
2023-05-09 19:33:50 -0400

    added code to html to make api work in browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,4 @@ var topic = "cats";
 
 var queryUrl = "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=" + topic + "apiKey=" + apiKey;
 
-fetch(https://newsapi.org/v2/everything?q=apple&from=2023-05-08&to=2023-05-08&sortBy=popularity&apiKey=633e9fc1f7394f1a90b345edcdbd4892)
+fetch("https://newsapi.org/v2/everything?q=apple&from=2023-05-08&to=2023-05-08&sortBy=popularity&apiKey=633e9fc1f7394f1a90b345edcdbd4892")

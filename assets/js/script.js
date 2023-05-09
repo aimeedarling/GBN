54f5a9c48e59f113c32d573e302a240a967644e7
fishy <miah.nishat.227@gmail.com>
2023-05-09 18:55:51 -0400

    added test url for fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ var apiKey = "633e9fc1f7394f1a90b345edcdbd4892";
 
 var topic = "cats";
 
-var queryUrl = "https://newsapi.org/v2/everything?q=" + topic + "apiKey=" + apiKey;
+var queryUrl = "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=" + topic + "apiKey=" + apiKey;
 
-fetch(queryUrl)
+fetch(https://newsapi.org/v2/everything?q=apple&from=2023-05-08&to=2023-05-08&sortBy=popularity&apiKey=633e9fc1f7394f1a90b345edcdbd4892)

b5231e7efda3c1c3b2ac2443ab2d88f2f8312cfb
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-05-09 19:33:29 -0700

    changed name of css file, update to search bar location, and update to search button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,34 @@
 
 //function of search button
 
-let searchbtn = document.querySelector(".search-btn")
-let userInput = document.querySelector(".input")
-let savedContent 
+// let searchbtn = document.querySelector(".search-btn")
+// let userInput = document.querySelector(".input")
+// let savedContent
 
-searchbtn.addEventListener("click", function(){
-    let userSearch = userInput.value
+// searchbtn.addEventListener("click", function () {
+//     let userSearch = userInput.value
 
 
-})
+// })
+
+$(function () {
+    $('.search-btn').click(function (e) {
+        e.preventDefault()
+        let userSearch = $('#searchText').val
+        let searches = JSON.parse(localStorage.getItem('searches')) || [];
+     
+        searches.push(userSearch)
+
+        let updatedSearches = JSON.stringify(searches)
+
+        localStorage.setItem('searches', JSON.stringify(userSearch));
+
+    });
+
+    $('.search-history').each(function(){
+        if (userSearches){
+            $(this).find('li').cal(searches)
+        }
+    })
+
+});

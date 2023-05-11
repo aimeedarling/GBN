8c55c6d7c85ef8afe4fcbb285a252cd9aab51231
fishy <miah.nishat.227@gmail.com>
2023-05-11 19:27:44 -0400

    deleted sample js code from newsapi site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,3 @@
-/* apikey = 'c167ea3e7370c8a8771c880aa1c0d815';
-topic = "cats"
-url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apikey;
-
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    articles = data.articles;
-
-    for (i = 0; i < articles.length; i++) {
-      // articles[i].title
-      console.log("Title: " + articles[i]['title']);
-      // articles[i].description
-      console.log("Description: " + articles[i]['description']);
-      // You can replace {property} below with any of the article properties returned by the API.
-      // articles[i].{property}
-      // console.log(articles[i]['{property}']);
-
-      // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-      break;
-    }
-  }); */
 
 //Code to include newsAPI
 

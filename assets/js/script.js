ba7b2b97653c6acf337483d000beafd4ad6bfd46
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-05-10 19:11:55 -0700

    current issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,51 @@
 
 
-
-
-//function of search button
-
-// let searchbtn = document.querySelector(".search-btn")
-// let userInput = document.querySelector(".input")
-// let savedContent
-
-// searchbtn.addEventListener("click", function () {
-//     let userSearch = userInput.value
-
-
-// })
-
 $(function () {
+   
+    let userSearch = $('#user-input').val();
+
     $('.search-btn').click(function (e) {
-        e.preventDefault()
-        let userSearch = $('#search-text').val()
-        let searches = JSON.parse(localStorage.getItem(searches)) || [];
-     
+        e.preventDefault();
+        let storedSearches = localStorage.getItem('searches')
+        console.log(storedSearches)
+        let searches = JSON.parse(localStorage.getItem('userSearch') || []);
         searches.push(userSearch)
 
         let updatedSearches = JSON.stringify(searches)
 
-        localStorage.setItem('searches', JSON.stringify(userSearch));
+        localStorage.setItem('searches', updatedSearches);
         console.log(userSearch)
     });
 
     $('.search-history').each(function(){
-        if (userSearches){
-            $(this).append('<li>.val(searches)</li>')
+        let searches = JSON.parse(localStorage.getItem('userSearch'))
+        if (userSearch){
+            $(this).append($('<li>').text(searches));
         }
     })
 
 });
+
+
+// $(function () {
+//     $('.search-btn').click(function (e) {
+//         e.preventDefault();
+//         let userSearch = $('#search-text').val();
+//         let searches = JSON.parse(localStorage.getItem('searches')) || [];
+
+//         searches.push(userSearch);
+
+//         let updatedSearches = JSON.stringify(searches);
+
+//         localStorage.setItem('searches', updatedSearches);
+//         console.log(userSearch);
+//     });
+
+//     $('.search-history').each(function () {
+//         let searches = JSON.parse(localStorage.getItem('searches')) || [];
+
+//         searches.forEach(function (search) {
+//             $(this).append($('<li>').text(search));
+//         }, this);
+//     });
+// });

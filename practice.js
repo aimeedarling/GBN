82d79f8241b4a756849609927e0b7fd3740d7667
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-05-15 18:36:44 -0700

    working search

--- practice.js
+++ practice.js
@@ -87,3 +87,12 @@ $(document).ready(function () {
     });
 
 });
+
+
+
+//function of search button
+
+const searchbtn = document.querySelector(".search-btn")
+let userInput = document.querySelector(".input")
+
+
